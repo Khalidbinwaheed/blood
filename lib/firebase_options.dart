4477798545cdbd84ce826ce7656f8548ad121997
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-03-31 23:44:32 +0500

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -7,7 +7,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart';
 
-show defaultTargetPlatform, kIsWeb, TargetPlatform;
+// Removed invalid 'show' statement.
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
